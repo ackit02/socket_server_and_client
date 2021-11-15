09b61a7ad7a6b2e60c79d3d1f256ca60b92f7f53
ackit02 <72196685+ackit02@users.noreply.github.com>
2021-11-15 03:50:54 +0300

    Just add some comments in code

--- libary.py
+++ libary.py
@@ -1,21 +1,27 @@
 import threading
 import socket
 import random
+#this is function for theard, that mean that it should work multiply times
 def connection(conne,addr):
     new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#I'm trying binding excited socket with my local ip and random port
     while 1:
         try:
             new_socket.bind(("192.168.1.23", random.randint(1001, 65335)))
             break
         except:
             None
+#Server sending this message to make show, that server is ready to recv new port from client 
     conne.send("ok".encode("UTF-8"))
     port = int(conn.recv(1024).decode("UTF-8"))
     address = (addr[0], port)
+#server making new socket for future connecting with client and to make free excited socket
     new_socket.connect(address)
     nickname = new_socket.recv(1024).decode("UTF-8")
+#Close connecting with "old" socket, now server is ready to accept new connection
     conn.close()
     print(f"User {nickname} already connected")
+#This variable show that function is already close connecting
     global x
     x = 1
     while 1:
@@ -28,10 +34,13 @@ x = 0
 while 1:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind(("192.168.1.23", 8000))
+#Server make standart socket with constant port
     sock.listen(5)
     conn, addr = sock.accept()
+#Now new theard taking socket and function "connection"
     xs = threading.Thread(target=connection, args=(conn, addr))
     xs.start()
+#With loop Server waiting before it can accept new connection 
     while 1:
         if x == 1:
             x = 0
