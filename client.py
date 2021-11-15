c2eaf17d17eb1ef50482959fa6c97f1c779fb840
ackit02 <72196685+ackit02@users.noreply.github.com>
2021-11-15 03:57:59 +0300

    Add some commentars in code

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 import random
+#Making standart socket and trying to bind it with local ip and random port
 old_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 while 1:
     try:
@@ -8,8 +9,10 @@ while 1:
     except:
         None
 old_socket.connect(("192.168.1.23", 8000))
+#Making new socket for future connection
 new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 random_port = 0
+#Same way to binding as first socket
 while 1:
     try:
         random_port = random.randint(1001, 65535)
@@ -17,10 +20,18 @@ while 1:
         break
     except:
         None
-if old_socket.recv(1024).decode("UTF-8") == 'ok':
-    old_socket.send(str(random_port).encode("UTF-8"))
+#Waiting when server will be ready for future connection
+while 1:
+    if old_socket.recv(1024).decode("UTF-8") == 'ok':
+    #Sending port of new socket for connection
+        old_socket.send(str(random_port).encode("UTF-8"))
+        break
+    else:
+        None
+#Waiting when server will connection to client
 new_socket.listen(5)
 conn, addr = new_socket.accept()
+#Just send nickname for to test how server work
 conn.send("Sonya".encode("UTF-8"))
 while 1:
     conn.send(input(">").encode("UTF-8"))
